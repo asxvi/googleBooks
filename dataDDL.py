4d0d9c2a0a2013129c45d91af90d956cb6fe43c8
asxvi <astanson02@gmail.com>
2025-03-29 10:52:05 -0500

    insertData functionality

--- dataDDL.py
+++ dataDDL.py
@@ -11,46 +11,61 @@ pg_password = os.getenv('PG_PASSWORD')
 # https://www.psycopg.org/psycopg3/docs/basic/usage.html
 # https://www.postgresql.org/docs/current/ddl.html
 
-def createTables():
-    with psycopg.connect(f"dbname=booksdb user={pg_username} password={pg_password} host={pg_host} port=5432") as conn:
-        with conn.cursor() as cursor:
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS book (
-                        bookId SERIAL PRIMARY KEY,
-                        isbn BIGINT UNIQUE NOT NULL,
-                        date DATE,
-                        pageCount SMALLINT,
-                        largeImage VARCHAR(100),
-                        smallImage VARCHAR(100),
-                        selfLink VARCHAR(100) NOT NULL,
-                        ebook BOOLEAN,
-                        epub BOOLEAN,
-                        pdf BOOLEAN
-                    );
+def connectDB():
+    return (psycopg.connect(f"dbname=booksdb user={pg_username} password={pg_password} host={pg_host} port=5432"))
 
-                CREATE TABLE IF NOT EXISTS author (          
-                        authorId SERIAL PRIMARY KEY,
-                        name TEXT NOT NULL UNIQUE
-                    );
+
+def createTables():
+    conn = connectDB()
+    cursor = conn.cursor()     
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS book (
+                bookId SERIAL PRIMARY KEY,
+                isbn BIGINT UNIQUE NOT NULL,
+                date DATE,
+                pageCount SMALLINT,
+                largeImage VARCHAR(100),
+                smallImage VARCHAR(100),
+                selfLink VARCHAR(100) NOT NULL,
+                ebook BOOLEAN,
+                epub BOOLEAN,
+                pdf BOOLEAN
+            );
+
+        CREATE TABLE IF NOT EXISTS author (          
+                authorId SERIAL PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE
+            );
+        
+        CREATE TABLE IF NOT EXISTS book_author (
+                book_authorId SERIAL PRIMARY KEY,
+                authorId INT REFERENCES author(authorId) ON DELETE CASCADE,
+                bookId INT REFERENCES book(bookId) ON DELETE CASCADE            
+            );
                 
-                CREATE TABLE IF NOT EXISTS book_author (
-                        book_authorId SERIAL PRIMARY KEY,
-                        authorId INT REFERENCES author(authorId) ON DELETE CASCADE,
-                        bookId INT REFERENCES book(bookId) ON DELETE CASCADE            
-                    );
-                        
-                CREATE TABLE IF NOT EXISTS category (
-                        categoryId SERIAL PRIMARY KEY,
-                        name TEXT NOT NULL UNIQUE     
-                    );
-                        
-                CREATE TABLE IF NOT EXISTS book_category (
-                        book_categoryId SERIAL PRIMARY KEY,     
-                        bookId INT REFERENCES book(bookId) ON DELETE CASCADE,
-                        categoryId INT REFERENCES category(categoryId) ON DELETE CASCADE
-                    );
-            ''')
-        conn.commit()
+        CREATE TABLE IF NOT EXISTS category (
+                categoryId SERIAL PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE     
+            );
+                
+        CREATE TABLE IF NOT EXISTS book_category (
+                book_categoryId SERIAL PRIMARY KEY,     
+                bookId INT REFERENCES book(bookId) ON DELETE CASCADE,
+                categoryId INT REFERENCES category(categoryId) ON DELETE CASCADE
+            );
+    ''')
+    conn.commit()
+
+def insertData(data=[]):
+    conn = connectDB()
+    cursor = conn.cursor()     
+    
+    cursor.execute('''
+        
+
+
+    ''')
 
 
     
